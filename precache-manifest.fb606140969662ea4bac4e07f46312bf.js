edf05ecb396d0ff78093adfc8a8bb33b61f580c2
Vaasu Dhand <dhandvaasu@gmail.com>
2020-06-19 23:08:17 -0400

    Updates

---
+++ precache-manifest.fb606140969662ea4bac4e07f46312bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6094015cc3eaf3c9ca556befa28ff618",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c6e9bc1b874ce072349",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5150e3f61b8addc8a354",
+    "url": "/static/js/2.fad596ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fad596ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c6e9bc1b874ce072349",
+    "url": "/static/js/main.5beb19dd.chunk.js"
+  },
+  {
+    "revision": "329586067db11da9de8d",
+    "url": "/static/js/runtime-main.d82a02e0.js"
+  }
+]);
